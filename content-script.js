8730297837934537bdbbe78c2f55a94b633ada46
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-02-22 23:29:24 +0100

    Add logging

--- content-script.js
+++ content-script.js
@@ -1,3 +1,21 @@
+console.image = function(url, size = 100) {
+    var image = new Image();
+    image.onload = function() {
+      var style = [
+        'font-size: 1px;',
+        'padding: ' + this.height/100*size + 'px ' + this.width/100*size + 'px;',
+        'background: url(' + url + ') no-repeat;',
+        'background-size: contain;'
+       ].join(' ');
+       console.log('%c ', style);
+    };
+
+    image.src = url;
+};
+
+console.image("https://wows-karma.com/assets/media/icons/android-chrome-512x512.png", 50);
+console.log("Loaded WOWS Karma for Chromium.")
+
 let a = document.createElement("a");
 a.target = "_blank";
 a.href = window.location.href.replace("numbers", "karma");
@@ -9,3 +27,5 @@ img.style = "height: 1.17em; margin-left: 0.5em;"
 
 a.appendChild(img);
 document.querySelector("body > div.container.main-container > div:nth-child(4) > div > h3").appendChild(a);
+
+console.debug("Appended WOWS Karma link.");
