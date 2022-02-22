b3a05d2a85ead2fe770093fbbb6478fdfa67ca1e
Scorpionfly31 <Scorpionfly31@gmail.com>
2022-02-22 20:34:10 +0000

    Add WOWS Karma Icon to link

--- content-script.js
+++ content-script.js
@@ -1,5 +1,11 @@
 let a = document.createElement("a");
+a.target = "_blank";
 a.href = window.location.href.replace("numbers", "karma");
-a.textContent = "WOWS Karma";
-a.target = "_blank"
+
+let img = document.createElement("img");
+img.src = "https://wows-karma.com/assets/media/icons/android-chrome-192x192.png";
+img.alt = "Open WOWS Karma Profile";
+img.style = "height: 1.17em; margin-left: 0.5em;"
+
+a.appendChild(img);
 document.querySelector("body > div.container.main-container > div:nth-child(4) > div > h3").appendChild(a);
